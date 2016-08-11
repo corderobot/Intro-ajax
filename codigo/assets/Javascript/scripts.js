6a1a855b9f39683e770f83852826b5dbe3b7e17d
corderobot <rodriscor@gmail.com>
2016-08-11 08:22:57 -0600

    imagenes
    Se le han puesto algunas imágenes y se han empezado a mostrar los datos
    que se mandan a llamar

--- codigo/assets/Javascript/scripts.js
+++ codigo/assets/Javascript/scripts.js
@@ -5,6 +5,7 @@ var humidity;
 var city;
 var temp_max;
 var temp_min;
+var img;
 
 var apiKey = "6efc7e595cf4af1d33d6d33aac14616a";
 var country = "Guatemala";
@@ -23,11 +24,17 @@ $(document).ready(function(){
 				city = (msj["city"]["name"]);
 				lat = (msj["city"]["coord"]["lat"]).toFixed(2);
 				lon = (msj["city"]["coord"]["lon"]).toFixed(2);
-				
-				clima = (msj["list"][0]["weather"][0]["description"]);
-				humidity = (msj["list"][0]["humidity"]);
-				temp_max = (msj["list"][0]["temp"]["max"]).toFixed(2);
-				temp_min = (msj["list"][0]["temp"]["min"]).toFixed(2);
+				$("#titulo").html(city);
+				$("#latitud").html("Latitud: " + lat);
+				$("#longitud").html("Longitud: " + lon);
+				for (var i = 0; i < 7; i++) {
+					img = (msj["list"][0]["weather"][0]["main"]);
+					clima = (msj["list"][0]["weather"][0]["description"]);
+					humidity = (msj["list"][0]["humidity"]);
+					temp_max = (msj["list"][0]["temp"]["max"]).toFixed(2);
+					temp_min = (msj["list"][0]["temp"]["min"]).toFixed(2);	
+					$("#dia_" + i).prepend('<img src="assets/img/" + img + ".jpg" + class="pequeno">');
+				}
 				console.log(temp_max);
 			}	
 		});
