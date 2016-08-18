4997a7630a7717af1dbb2e8ee23ebb6fe8f33cee
corderobot <rodriscor@gmail.com>
2016-08-18 07:11:41 -0600

    Interaccion
    El usuario ya puede interactuar con la página haciendole click a los
    divs miniatura para poder verlos en grande e ingresar su ciudad

--- codigo/assets/Javascript/scripts.js
+++ codigo/assets/Javascript/scripts.js
@@ -8,28 +8,34 @@ var temp_min;
 var img;
 var nubes;
 var viento;
-var hoy = new Date();
-var dd = hoy.getDate();
-var mm = hoy.getMonth()+1; //Enero es 0
-var yyyy = hoy.getFullYear();
+var $miniatura = $('#miniatura');
+var $principal = $('#principal');
+var p = '<p class="texto2 margen">';
+
 
 var apiKey = "6efc7e595cf4af1d33d6d33aac14616a";
 var apiURL = "http://api.openweathermap.org/data/2.5/forecast/daily?q=";
 
 $(document).ready(function(){
-	for (var i = 0; i<7; i++) {
-		$(".dia_" + i).on('click', function(){
-			alertar(i);
+
+	function alertar(num){
+		$(".dia_" + num).on('click', function(){
+			for (var i = 0; i < 7; i++) {
+				$('#grande.dia_' + i).hide();
+			};
+			$('#grande.dia_' + num).show("slow");
 		});
 	}
 
-	function alertar(num){
-		console.log(num);
-	}
-
 	$("#refresh").on("click", function(){
-		var country = $("#city").val();
-		country = "guatemala";
+		var city = $("#city").val();
+		var country = $("#country").val();
+		
+		var hoy = new Date();
+		var dd = hoy.getDate();
+		var mm = hoy.getMonth()+1; //Enero es 0
+		var yyyy = hoy.getFullYear();
+
 		if(dd<10) {
 		    dd='0'+dd;
 		} 
@@ -37,19 +43,23 @@ $(document).ready(function(){
 		    mm='0'+mm;
 		} 
 
-		var apiRoute = apiURL + country + "&units=metric&cnt=7&appid=" + apiKey;
+
+		var apiRoute = apiURL + city + "," + country + "&units=metric&cnt=7&appid=" + apiKey;
+		
 		$.ajax({
 			method: 'GET',
 			url: apiRoute,
 			success: function(msj, estado, xhr){
 				console.log(msj);
-				console.log(estado);
 				city = (msj["city"]["name"]);
+				country = (msj["city"]["country"]);
 				lat = (msj["city"]["coord"]["lat"]).toFixed(2);
 				lon = (msj["city"]["coord"]["lon"]).toFixed(2);
-				$("#titulo").html(city);
+				$("#titulo").html(city + "," + country);
 				$("#latitud").html("Latitud: " + lat);
 				$("#longitud").html("Longitud: " + lon);
+				$miniatura.html('');
+				$principal.html('');
 				for (var i = 0; i < 7; i++) {
 					hoy = dd+'/'+mm+'/'+yyyy;
 					$(".dia_" + i).html("");
@@ -60,15 +70,22 @@ $(document).ready(function(){
 					humidity = (msj["list"][i]["humidity"]);
 					temp_max = (msj["list"][i]["temp"]["max"]).toFixed(2);
 					temp_min = (msj["list"][i]["temp"]["min"]).toFixed(2);	
-					$(".dia_" + i).prepend('<p class="texto2 margen">' + "Nubes: " + nubes + '</p>');
-					$(".dia_" + i).prepend('<p class="texto2 margen">' + "Humedad: " + humidity + '</p>');
-					$(".dia_" + i).prepend('<p class="texto2 margen">' + "Temperatura minima: " + temp_min + 'C</p>');
-					$(".dia_" + i).prepend('<p class="texto2 margen">' + "Temperatura maxima: " + temp_max + 'C</p>');
+					$miniatura.append('<div class="dias right"> <div class="mini dia_' + i + '"</div> </div>');
+					$principal.append('<div id="grande" class="dia_' + i + '"> </div>');
+					$(".dia_" + i).prepend(p + "Nubes: " + nubes + '</p>');
+					$(".dia_" + i).prepend(p + "Humedad: " + humidity + '</p>');
+					$(".dia_" + i).prepend(p + "Temperatura minima: " + temp_min + 'C</p>');
+					$(".dia_" + i).prepend(p + "Temperatura maxima: " + temp_max + 'C</p>');
 					$(".dia_" + i).prepend('<img class="margen" src="assets/img/' + img + '.jpg">');
-					$(".dia_" + i).prepend('<p class="texto2 margen">' + "Viento: " + viento + 'm/s</p>');
-					$('.dia_' + i).prepend('<p class="texto center">' + clima + '</p>');
-					$('.dia_' + i).prepend('<p class="texto center">' + hoy + '</p>');
+					$(".dia_" + i).prepend(p + "Viento: " + viento + 'm/s</p>');
+					$('.dia_' + i).prepend('<p class="texto center  font_type">' + clima + '</p>').css('textTransform', 'capitalize');
+					$('.dia_' + i).prepend('<p class="texto center font_type">' + hoy + '</p>');
+					alertar(i);
 					dd++;
+					if (dd == 32) {
+						dd = 1;
+						mm++;
+					};
 				}
 			}	
 		});
